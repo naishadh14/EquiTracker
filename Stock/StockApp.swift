767d2883ae92e68729cc087f67f69a52a74d2b23
Naishadh Vora <voranaishadh@gmail.com>
2024-04-25 00:04:15 -0700

    Initial Commit

---
+++ Stock/StockApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockApp.swift
+//  Stock
+//
+//  Created by Naishadh Vora on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct StockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
